8aa1b6738a6d8da7c04ce521b45440cfaea10b56
biplobsec21 <biplobsec21@gmail.com>
2026-01-31 23:45:07 +0600

    Deleted log

--- application/logs/log-2026-01-31.php
+++
@@ -1,5 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2026-01-31 16:40:15 --> Query error: Table 'c_attendance.db_sitesettings' doesn't exist - Invalid query: SELECT a.currency_name,a.currency,a.currency_code,a.symbol,b.currency_placement FROM db_currency a,db_sitesettings b WHERE a.id=b.currency_id AND b.id=1
-ERROR - 2026-01-31 16:40:15 --> Severity: error --> Exception: Call to a member function row() on bool /Users/hello/Herd/21eb.ramksofttech.com/application/core/MY_Controller.php 22
-ERROR - 2026-01-31 16:48:20 --> Severity: error --> Exception: syntax error, unexpected token "continue" /Users/hello/Herd/21eb.ramksofttech.com/system/database/DB_query_builder.php 2424
